865eb846cc75fd68e52ee3f1336cb131ed0409fc
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2025-11-15 10:41:56 +0100

    Button component added

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,11 +1,15 @@
+import Button from "@/components/Button"
 import Card from "@/components/Card"
 
 const Landing: React.FC =  () => {
   return (
     <div>
         <Card />
+      
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+        <Button/>
     </div>
+    
   )
 }
 export default Landing
